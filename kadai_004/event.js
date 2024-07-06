3ec279e4ca4925547accba9a359066559a430d79
nori3 <norito@toralab.org>
2024-07-06 18:38:59 +0900

    load2

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -6,10 +6,17 @@
 //         console.log('scrollイベントが発生しました')
 //     });
 // });
+console.log('test');
+
+$(window).on('load', () => {
+    console.log('loadイベントが発生しました');
+});
 
 $(function() {
+    console.log('test2');
     $(window).on({
-        'load': () => {console.log('loadイベントが発生しました')},
+        // 'load': () => {console.log('loadイベントが発生しました')},
         'scroll': () => {console.log('scrollイベントが発生しました')},    
     });
+    
 });
